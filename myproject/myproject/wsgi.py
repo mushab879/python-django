550000ceddcdd971d1438865ba4992d5e27516d2
mushab879 <mmushab37@gmail.com>
2025-08-31 13:40:38 +0530

    Update wsgi.py

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -5,8 +5,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
 
-# Ensure the application binds to the correct host and port
-if os.environ.get('PORT'):
-    application.run(host='0.0.0.0', port=int(os.environ['PORT']))
 
 
